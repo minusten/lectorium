b920f3f586a05e0a5994084f9a12a774ba893a6d
lucianasiegel <lovebigdaddy303@gmail.com>
2018-11-01 11:50:31 +0200

    second homework changes

--- second_lesson/script.js
+++ second_lesson/script.js
@@ -1,30 +1,29 @@
-// var arr = [-1, 3, 5, 7, -4, 0]
-// var arr = [0, 4, -8, undefined, 3, 5, NaN]
 
-function max(arr){
+
+function max (arr) {
   var max = arr[0]
-  for (var i = 0; i < arr.length; i++){
+  for (var i = 0; i < arr.length; i++) {
     if (arr[i] > max) max = arr[i]
   }
   return max
 }
 console.log(max([-1, 3, 5, 7, -4, 0]))
-console.log(max([0, 4, -8, undefined, 3, 5, NaN]));
+console.log(max([0, 4, -8, undefined, 3, 5, NaN]))
 
-function min(arr){
+function min (arr) {
   var min = arr[0]
-  for (var i = 0; i < arr.length; i++){
+  for (var i = 0; i < arr.length; i++) {
       if (arr[i] < min) min = arr[i]
   }
   return min
 }
 console.log(min([-1, 3, 5, 7, -4, 0]))
-console.log(min([0, 4, -8, undefined, 3, 5, NaN]));
+console.log(min([0, 4, -8, undefined, 3, 5, NaN]))
 
-function sum(arr){
+function sum (arr) {
   var sum = 0
-  for (var i = 0; i < arr.length; i++){
-    if (typeof arr[i] === 'undefined' || isNaN(arr[i])){
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'undefined' || isNaN(arr[i])) {
       continue;
     }
     sum = sum + arr[i]
@@ -32,4 +31,4 @@ function sum(arr){
   return sum
 }
 console.log(sum([-1, 3, 5, 7, -4, 0]))
-console.log(sum([0, 4, -8, undefined, 3, 5, NaN]));
+console.log(sum([0, 4, -8, undefined, 3, 5, NaN]))
