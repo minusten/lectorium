1f939553aee952690ffaccfa57007ec2978bc478
lucianasiegel <lovebigdaddy303@gmail.com>
2018-12-07 14:55:38 +0200

    changed 1 task

--- third_lesson/script.js
+++ third_lesson/script.js
@@ -1,39 +1,37 @@
 // -----------------------ЗАДАНИЕ № 1--------------------
 
-var arr =  [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8] 
-
-  var currentEndMount = findCurrentEndMount(arr)
-  var currentStartMount = arr[0], water = 0;
-  for(var i = 1; i < currentEndMount; i++) {
-    if (arr[i] > currentStartMount) {
-      currentStartMount = arr[i]
+var arr =  [2, 2, 2, 2, 2] 
+  var maxLastInd = findMaxLastInd(arr)
+  var currentMax = arr[0], water = 0
+  for(var i = 1; i < maxLastInd; i++) {
+    if (arr[i] > currentMax) {
+      currentMax = arr[i]
     }
     else {
-      water = (water + currentStartMount) - arr[i]
+      water = (water + currentMax) - arr[i]
     }
   }
-  currentStartMount = arr[arr.length - 1]
-  for(var i = arr.length - 1; i> currentEndMount; i--) {
-    if (arr[i] > currentStartMount) {
-      current_max = arr[i];
+  currentMax = arr[arr.length - 1]
+  for(var i = arr.length - 1; i > maxLastInd; i--) {
+    if (arr[i] > currentMax) {
+      currentMax = arr[i]
     }
     else {
-    water = (water + currentStartMount) - arr[i]
+      water = (water + currentMax) - arr[i]
     }
   }
   console.log(water)
 
 
-function findCurrentEndMount(arr) {
-  return arr.reduce(function(sum, currentValue, currentIndex, arr) {
-    if (currentValue >= arr[sum]) {
-    return currentIndex
-  } else {
-    return sum
+  function findMaxLastInd(arr) {
+    return arr.reduce(function(sum, currentValue, currentIndex, arr) {
+      if (currentValue >= arr[sum]) {
+      return currentIndex
+    } else {
+      return sum
+    }
+    })
   }
-  })
-}
-
 // --------------------ЗАДАНИЕ № 2--------------------
 ﻿var arr = ['Новый', 'Год', 'будет', 31, 'января']
 function convertFromArrToObj (arr, obj) {
