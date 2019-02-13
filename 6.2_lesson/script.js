79134180b9c036b244754a62b3895a50bfd6c211
lucianasiegel <lovebigdaddy303@gmail.com>
2019-02-13 13:10:26 +0200

    changed 6.2

--- 6.2_lesson/script.js
+++ 6.2_lesson/script.js
@@ -1,21 +1,20 @@
 // isPrime
 function isPrime (num) {
   for (let i = 2; i < num; i++)
-    if (num % i === 0) return false;
+    if (num % i === 0) {
+      return false;
+    }
   return num !== 1 && num !== 0;
 }
 console.log(isPrime(17))
 
 //factorial
-var f = [];
-function factorial (num) {
-  if (num == 0 || num == 1)
-    return 1;
-  if (f[num] > 0)
-    return f[num];
-  return f[num] = factorial(num - 1) * num;
+
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
 }
-console.log (factorial(6))
+
+console.log (factorial(5))
 
 //fibonacci
 function fib(num) {
@@ -31,18 +30,21 @@ function fib(num) {
 console.log(fib(7))
 
 //isSorted
+
+var sortArray = [1, 2, 3, 4]
 function isSorted(array) {
-  var array = [-Infinity, -5, 0, 3, 9];
   for (a = 0; a < array.length; a++) {
     for (b = 0; b < a; b++) {
-       if (array[b] > array[a]) return false;
+       if (array[b] > array[a]) {
+         return false;
+       }
     }
     for (b = a + 1; b < array.length; b++) {
        if (array[b] < array[a]) return false;
     }
   }
   return true;
-} console.log(isSorted())
+} console.log(isSorted(sortArray))
 
 //reverse
 function reverse(str) {
@@ -54,25 +56,42 @@ function reverse(str) {
 } console.log(reverse("hello"))
 
 //indexOf
-var arr = ['f', 2, 'a', 4];
-console.log(arr.indexOf("a", 1))
+var arr = [1, 2, 3, 4];
+Array.prototype.myIndexOf = function (elem) {
+  for (var i = 0; i < this.length; i++){
+    if (this[i] === elem) {
+      return i
+    }
+} return -1
+}
+console.log (['e', 'f', 'a'].myIndexOf('f'))
+
+
 
 //isPalindrome
 function isPalindrome(str) {
-    return str == str.split('').reverse().join('');
+    return str === str.split('').reverse().join('');
 }
 console.log(isPalindrome("eye"));
 
 //missing
-var a = [5, 10], count = 10;
-var missing = new Array();
+var numbers = [0,1,3,4,5,7,8];
 
-for(var i = 1; i <= count; i++) {
-    if(a.indexOf(i) == -1) {
-        missing.push(i);
-    }
+function isMissing (numbers) {
+  var missing = [];
+
+
+  for ( var i = 0; i < numbers.length; i++ ) {
+
+      if ( (numbers[i+1] - numbers[i]) > 1 ) {
+          missing.push( numbers[i+1] - numbers[1] );
+      }
+  }
+
+  console.log( missing );
 }
-console.log(missing);
+
+isMissing(numbers)
 
 //isBalanced
 const isBalanced = (string) => {
